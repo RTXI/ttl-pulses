cb5b7dcc15cf73df348e71d9ce592762d1ad5959
Yogi <y.patel@me.com>
2015-01-29 15:33:13 -0500

    adding window resize timer

--- ttl.h
+++ ttl.h
@@ -19,29 +19,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <default_gui_model.h>
 #include <string>
 
-class TTL : public DefaultGUIModel{
-	
-	public:
-		TTL(void);
-		virtual ~TTL(void);
-		virtual void execute(void);
-	
-	protected:
-		virtual void update(DefaultGUIModel::update_flags_t);
-	
-	private:
-		double dt;
-		double ttlDuration; // s
-		double ttlNumPulses;
-		double ttlFreq; // Hz
-		double ttlDelay; // s
-		double maxtrials;
-		int trial;
-		int idx;
-		std::vector<double> stimTTL;
-	
-	private slots:
-		void initParameters();
-		void bookkeep();
-		void makeTTL();
+class TTL : public DefaultGUIModel
+{
+
+public:
+    TTL(void);
+    virtual ~TTL(void);
+    virtual void execute(void);
+
+protected:
+    virtual void update(DefaultGUIModel::update_flags_t);
+
+private:
+    double dt;
+    double ttlDuration; // s
+    double ttlNumPulses;
+    double ttlFreq; // Hz
+    double ttlDelay; // s
+    double maxtrials;
+    int trial;
+    int idx;
+    std::vector<double> stimTTL;
+
+private slots:
+    void initParameters();
+    void bookkeep();
+    void makeTTL();
 };
